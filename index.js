da5742729aa200da6e810e59aa727c7667ab41cb
Simon Dunn <simondunn90@gmail.com>
2023-12-14 14:07:34 +0000

    use node

---
+++ index.js
@@ -0,0 +1,9 @@
+import { readFileSync } from 'node:fs'
+const input = readFileSync('./input.txt', 'utf8')
+
+function adventOfCode (input) {
+  // solve the challenge here
+  return input
+}
+
+console.log(adventOfCode(input))
